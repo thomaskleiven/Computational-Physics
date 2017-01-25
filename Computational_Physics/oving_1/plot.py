8e6a24a5120c0fa115e1fbf1e7b4f728c31d9d24
Thomas Kleiven <tkleiven@dhcp-10-22-67-67.wlan.ntnu.no>
2017-01-25 11:14:42 +0100

    Working plotting

--- Computational_Physics/oving_1/plot.py
+++ Computational_Physics/oving_1/plot.py
@@ -3,6 +3,7 @@ import matplotlib.mlab as mlab
 import csv
 import numpy as np
 import math as mp
+from scipy import stats
 
 x = []
 y = []
@@ -10,12 +11,18 @@ y = []
 with open('data.txt','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        if(np.float(row[0]) == 100000):
-            continue
         y.append(np.float(row[0]))
 
 
 x = np.arange(len(y))
+
+#plt.hist(y, bins = 10000)
+#plt.xlim([0,2000])
+#plt.ylim([0,30000])
+#plt.yscale('log')
+#plt.figure(2)
+#plt.xscale('log')
+#plt.show()
 
 
 #Plot cumultative distributin
@@ -24,6 +31,31 @@ plt.yscale('log')
 plt.xscale('log')
 plt.plot(base[:-1], values, c='green', ls="dashdot")
 plt.xlim([0,100000])
+
+print (len(base), len(values))
+
+y_min = 500
+y_max = 10000
+base_min = np.min(base)
+base_max = np.max(base)
+
+
+values = values[base < y_max]
+base = base[base < y_max]
+values = values[base > y_min]
+base = base[base > y_min]
+
+slope, intercept, rvalue, pvalue, stdr = stats.linregress(np.log(base), np.log(values))
+
+x = np.linspace(base_min, base_max, 100)
+fitted_line = np.exp(intercept)*x**slope
+print(slope)
+plt.plot(x, fitted_line)
+
+
+
+
+
 plt.show()
 
 
