111454745873a244eba8a780155df2da5570b8f3
Thomas Kleiven <tkleiven@dhcp-10-22-3-2.wlan.ntnu.no>
2017-02-06 10:35:57 +0100

    Implemented step diffusion

--- Computational_Physics/oving_1/Euler.cpp
+++ Computational_Physics/oving_1/Euler.cpp
@@ -16,7 +16,7 @@ static double dt = 0.1*dx*dx*1;           //Size of time steps
 static arma::vec u(nx);                   //unknown u at new time level
 static arma::vec u_l(nx);                 //u at previous time levelS
 
-static double F = dt/(dx*dx);
+//static double F = dt/(dx*dx);
 
 static arma::vec diagonal(nx);             //Diagonal
 static arma::vec subDiagonal(nx);          //SubDiagonal
@@ -25,6 +25,8 @@ static arma::vec diagonalB(nx);           //Diag and Subdiag for matrix B in Cra
 static arma::vec subDiagonalB(nx);
 
 static int counter = 0;
+
+static int xMin = -0.5;
 
 
 enum class Scheme_t{
@@ -38,32 +40,55 @@ public:
   }
 };
 
-void generateA(Scheme_t scheme){
-  switch (scheme) {
-    case Scheme_t::FORWARD_EULER:
-      u_l.fill(0);
-      u_l(nx/2) = 1;                 //Initial conditions
-      return;
+class StepDiffusion{
+public:
+  double operator()(double x){
+    if(x > 0){return 1.0;}
+    else{
+      return 0.5;
+    }
+  }
+};
 
-    case Scheme_t::BACKWARDS_EULER:
-      u.fill(0);
-      u(nx/2) = 1;                   //Initial condition
 
-      diagonal.fill(1 + 2*F);        //Set diagonal and subdiagonal
-      subDiagonal.fill(-F);
-      return;
-
-    case Scheme_t::CRANK_NICOLSON:
-      diagonal.fill(1+F);
-      subDiagonal.fill(-F/2);
-      diagonalB.fill(1-F);
-      subDiagonalB.fill(F/2);
-      u.fill(0);
-      u(nx/2)=1;
-      return;
-      }
+double getX(int i){
+  return xMin + i*dx;
 }
 
+
+template<class Diffusion>
+void generateA(Scheme_t scheme, Diffusion &D){
+  u_l.fill(0);
+  u.fill(0);
+  for(int i = 0; i<nx; i++){
+    double fPlus = D(getX(i+0.5))*dt/(dx*dx);
+    double fMinus = D(getX(i-0.5))*dt/(dx*dx);
+
+    switch (scheme) {
+
+      case Scheme_t::FORWARD_EULER:
+        break;
+
+      case Scheme_t::BACKWARDS_EULER:
+        diagonal(i) = 1 + fPlus + fMinus;        //Set diagonal and subdiagonal
+        subDiagonal(i) = -fMinus;
+        break;
+
+      case Scheme_t::CRANK_NICOLSON:
+        diagonal(i) = (1+0.5*fPlus+0.5*fMinus);
+        subDiagonal(i) = (-fMinus/2);
+        diagonalB(i) = (1-0.5*fPlus-0.5*fMinus);
+        subDiagonalB(i) = fMinus/2;
+        break;
+        }
+
+  }
+}
+
+
+void setInitialCondition(arma::vec &u){
+  u(nx/2) = 1;
+}
 
 
 
@@ -72,9 +97,13 @@ void generateA(Scheme_t scheme){
 
 void explicitEuler(bool reflective){
 
-  generateA(Scheme_t::FORWARD_EULER);
+  ConstantDiffusion D;
 
-  arma::mat results(nx, 5);
+  double F = D(0)*dt/(dx*dx);
+
+  generateA(Scheme_t::FORWARD_EULER, D);
+
+  arma::mat results(nx, nt);
 
   for (int i=0; i<nt;i++){
     for (int j=1; j<nx-1; j++){
@@ -86,9 +115,9 @@ void explicitEuler(bool reflective){
     }
 
     //Write to file every 5th iteration
-    if((i*5)%nt == 0){
+    //if((i*5)%nt == 0){
       results.col(counter++) = u;
-    }
+    //}
 
     //Update u_l before next step
     u_l = u;
@@ -96,13 +125,35 @@ void explicitEuler(bool reflective){
   results.save("explicitEuler.csv", arma::csv_ascii);
 }
 
+template<class Diffusion>
+void setReflectiveBoundriesForImplicitEuler(arma::vec &diag, Diffusion &D){
+
+  double F = D(0)*dt/(dx*dx);                               //Get F when X equals zero
+  diag(0) =1.0 + F;
+
+  F = D(getX(diag.n_elem-1))*dt/(dx*dx);                    //Get F when X euals nx-1
+  diag(diag.n_elem-1) = 1.0 + F;
+}
+
+void setReflectiveForCrank(arma::vec &diag, arma::vec &diagB, arma::vec &subdiagB){
+
+}
+
 
 void implicitEuler(bool reflective){
-  int info;                                                                                   //Has to be initialized in order to use LAPACK, returns SUCCESS or FAILURE
+  int info;
 
-  generateA(Scheme_t::BACKWARDS_EULER);
+  ConstantDiffusion D;                                                                                 //Has to be initialized in order to use LAPACK, returns SUCCESS or FAILURE
 
-  arma::mat results(nx, nt);
+  generateA(Scheme_t::BACKWARDS_EULER, D);                                                               //Generate matrix
+
+  setInitialCondition(u);
+
+  arma::mat results(nx, 5);
+
+  if(reflective){
+    setReflectiveBoundriesForImplicitEuler(diagonal, D);
+  }
 
   dpttrf_(&nx, diagonal.memptr(), subDiagonal.memptr(), &info);                               //Compute the symmetric system Ax = B using LAPACK
 
@@ -110,15 +161,11 @@ void implicitEuler(bool reflective){
   for(int i = 0; i<nt; i++){
     int nrhs = 1;
     dpttrs_(&nx, &nrhs, diagonal.memptr(), subDiagonal.memptr(), u.memptr(), &nx, &info);     // Solve the system Ax
-    if(reflective){
-      u(0) = u(1);
-      u(nx-1) = u(nx-2);
-    }
 
 
-    //if((i*5)%nt == 0){
+    if((i*5)%nt == 0){
       results.col(counter++) = u;
-    //}
+    }
 
   }
 
@@ -129,9 +176,16 @@ void implicitEuler(bool reflective){
 
   void crankNicolson(bool reflective){
     int info;
-    arma::mat results(nx, nt);
+    arma::mat results(nx, 5);
 
-    generateA(Scheme_t::CRANK_NICOLSON);
+    ConstantDiffusion D;
+
+    StepDiffusion S;
+
+    generateA(Scheme_t::CRANK_NICOLSON, S);
+
+    setInitialCondition(u);
+
 
     //Compute factorisation
     dpttrf_(&nx, diagonal.memptr(), subDiagonal.memptr(), &info);
@@ -141,17 +195,13 @@ void implicitEuler(bool reflective){
       for(int i=0; i < nx-2; ++i){
         u(i+1) = subDiagonalB(i)*u(i) + diagonalB(i+1)*u(i+1) + subDiagonalB(i+1)*u(i+2);
       }
-      if(reflective){
-        u(0) = u(1);
-        u(nx-1) = u(nx-2);
-      }
 
       //Solve the system Ax = Bb
       dpttrs_(&nx, &nrhs, diagonal.memptr(), subDiagonal.memptr(), u.memptr(), &nx,  &info);
 
-      //if((i*5)%nt == 0){
+      if((i*5)%nt == 0){
         results.col(counter++) = u;
-      //}
+      }
     }
 
     results.save("crankNicolson.csv", arma::csv_ascii);
@@ -167,8 +217,8 @@ void implicitEuler(bool reflective){
 
 int main(){
   //explicitEuler(true);
-  implicitEuler(true);
-  //crankNicolson(true);
+  //implicitEuler(true);
+  crankNicolson(false);
   return 0;
 }
 ;
