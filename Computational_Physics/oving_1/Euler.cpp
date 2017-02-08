57d19e4503d42c868e43ee573f7ed07d1c00ed66
Thomas Kleiven <tkleiven@localhost.localdomain>
2017-02-08 14:33:10 +0100

    Added non-constan diffusion

--- Computational_Physics/oving_1/Euler.cpp
+++ Computational_Physics/oving_1/Euler.cpp
@@ -3,30 +3,36 @@
 #include<armadillo>
 extern "C" void dpttrf_(int* n, double* diag, double* e, int* info);
 extern "C" void dpttrs_(int* n, int* nrhs, double* diag, double* subdiag, double* rhs, int* nx, int* info);
+#include <math.h>
+#include<sstream>
 
 using namespace std;
 
-//Declare variables
-static int nx = 40;                       //Number of length steps
-static int nt = 5000;                     //Number of time steps
 
-static double dx = 1.0/nx;                //Size of length steps
-static double dt = 0.1*dx*dx*1;           //Size of time steps
+
+//Declare variables
+static double endtime = 0.08;
+
+static int nx = 201;                       //Number of length steps
+
+static double dx = 1.0/(nx+1);                //Size of length steps
+static double dt = 1/1E5;           //Size of time steps
+
+static int nt = endtime/dt;
 
 static arma::vec u(nx);                   //unknown u at new time level
-static arma::vec u_l(nx);                 //u at previous time levelS
-
-//static double F = dt/(dx*dx);
+static arma::vec u_l(nx);                 //u at previous time levelS2
+static double F = dt/(dx*dx);
 
 static arma::vec diagonal(nx);             //Diagonal
-static arma::vec subDiagonal(nx);          //SubDiagonal
+static arma::vec subDiagonal(nx-1);          //SubDiagonal
 
 static arma::vec diagonalB(nx);           //Diag and Subdiag for matrix B in Crank Nicoloson
-static arma::vec subDiagonalB(nx);
+static arma::vec subDiagonalB(nx-1);
 
 static int counter = 0;
 
-static int xMin = -0.5;
+static float xMin = -0.5;
 
 
 enum class Scheme_t{
@@ -64,6 +70,9 @@ void generateA(Scheme_t scheme, Diffusion &D){
     double fPlus = D(getX(i+0.5))*dt/(dx*dx);
     double fMinus = D(getX(i-0.5))*dt/(dx*dx);
 
+    //cout << i*dx << " " <<  D(getX(i+0.5)) << " " << xMin << endl;
+    //cout << D(getX(i+0.5)) << endl;
+
     switch (scheme) {
 
       case Scheme_t::FORWARD_EULER:
@@ -71,14 +80,17 @@ void generateA(Scheme_t scheme, Diffusion &D){
 
       case Scheme_t::BACKWARDS_EULER:
         diagonal(i) = 1 + fPlus + fMinus;        //Set diagonal and subdiagonal
-        subDiagonal(i) = -fMinus;
+        if(i < nx-1){
+        subDiagonal(i) = -fMinus;}
         break;
 
       case Scheme_t::CRANK_NICOLSON:
         diagonal(i) = (1+0.5*fPlus+0.5*fMinus);
-        subDiagonal(i) = (-fMinus/2);
         diagonalB(i) = (1-0.5*fPlus-0.5*fMinus);
-        subDiagonalB(i) = fMinus/2;
+        if(i < nx-1){
+          subDiagonal(i) = (-fMinus/2);
+          subDiagonalB(i) = fMinus/2;
+          }
         break;
         }
 
@@ -87,7 +99,7 @@ void generateA(Scheme_t scheme, Diffusion &D){
 
 
 void setInitialCondition(arma::vec &u){
-  u(nx/2) = 1;
+  u(nx/2) = 1/dx;
 }
 
 
@@ -103,7 +115,9 @@ void explicitEuler(bool reflective){
 
   generateA(Scheme_t::FORWARD_EULER, D);
 
-  arma::mat results(nx, nt);
+  setInitialCondition(u_l);
+
+  arma::mat results(nx, 1);
 
   for (int i=0; i<nt;i++){
     for (int j=1; j<nx-1; j++){
@@ -115,14 +129,16 @@ void explicitEuler(bool reflective){
     }
 
     //Write to file every 5th iteration
-    //if((i*5)%nt == 0){
+    if(abs(0.07-i*dt) <1E-10){
       results.col(counter++) = u;
-    //}
+    }
 
     //Update u_l before next step
     u_l = u;
   }
-  results.save("explicitEuler.csv", arma::csv_ascii);
+  stringstream fname;
+  fname << "explicitEuler_" << static_cast<int>(dt*1E6) << ".csv";
+  results.save(fname.str().c_str(), arma::csv_ascii);
 }
 
 template<class Diffusion>
@@ -135,8 +151,16 @@ void setReflectiveBoundriesForImplicitEuler(arma::vec &diag, Diffusion &D){
   diag(diag.n_elem-1) = 1.0 + F;
 }
 
-void setReflectiveForCrank(arma::vec &diag, arma::vec &diagB, arma::vec &subdiagB){
+void setReflectiveForCrank(arma::vec &diag, arma::vec &diagB){
 
+  double F = dt/(dx*dx);
+  diag(0) = 1.0 + F/2;                         //Diagonal A
+
+  diag(diag.n_elem-1) = 1.0 + F/2;         //Diagonal A
+
+  diagB(0) = 1.0 - F/2;
+
+  diagB(diagB.n_elem-1) = 1.0 - F/2;
 }
 
 
@@ -149,7 +173,7 @@ void implicitEuler(bool reflective){
 
   setInitialCondition(u);
 
-  arma::mat results(nx, 5);
+  arma::mat results(nx, 1);
 
   if(reflective){
     setReflectiveBoundriesForImplicitEuler(diagonal, D);
@@ -163,52 +187,64 @@ void implicitEuler(bool reflective){
     dpttrs_(&nx, &nrhs, diagonal.memptr(), subDiagonal.memptr(), u.memptr(), &nx, &info);     // Solve the system Ax
 
 
-    if((i*5)%nt == 0){
+    if(abs(0.07-i*dt) <1E-20){
       results.col(counter++) = u;
     }
 
   }
-
-  results.save("implicitEuler.csv", arma::csv_ascii);
+  stringstream fname;
+  fname << "implicitEuler_" << static_cast<int>(dt*1E6) << ".csv";
+  results.save(fname.str().c_str(), arma::csv_ascii);
 }
 
 
 
   void crankNicolson(bool reflective){
     int info;
-    arma::mat results(nx, 5);
+    arma::mat results(nx, 1);
 
     ConstantDiffusion D;
 
     StepDiffusion S;
 
-    generateA(Scheme_t::CRANK_NICOLSON, S);
+    generateA(Scheme_t::CRANK_NICOLSON, D);
 
     setInitialCondition(u);
+
+    //cout << diagonalB(0) << " " << diagonalB(diagonalB.n_elem -1) << endl;
+
+    if(reflective){
+      setReflectiveForCrank(diagonal, diagonalB);
+    }
+
+    //cout << diagonalB(0) << " " << diagonalB(diagonalB.n_elem -1) << endl;
 
 
     //Compute factorisation
     dpttrf_(&nx, diagonal.memptr(), subDiagonal.memptr(), &info);
 
+
+
     for(int i = 0; i < nt; ++i){
       int nrhs = 1;
-      for(int i=0; i < nx-2; ++i){
-        u(i+1) = subDiagonalB(i)*u(i) + diagonalB(i+1)*u(i+1) + subDiagonalB(i+1)*u(i+2);
+      u_l = u;
+      for(int j=0; j < nx-2; ++j){
+        u(j+1) = subDiagonalB(j)*u_l(j) + diagonalB(j+1)*u_l(j+1) + subDiagonalB(j+1)*u_l(j+2);
       }
+      u(0) = u(0)*diagonalB(0) + subDiagonalB(0)*u(1);
+      u(u.n_elem-1) = u(u.n_elem-1)*diagonalB(diagonalB.n_elem-1) + u(u.n_elem-2)*subDiagonalB(subDiagonalB.n_elem-1);
 
       //Solve the system Ax = Bb
       dpttrs_(&nx, &nrhs, diagonal.memptr(), subDiagonal.memptr(), u.memptr(), &nx,  &info);
 
-      if((i*5)%nt == 0){
+      if(abs(0.002-i*dt) <1E-20){
         results.col(counter++) = u;
       }
     }
-
+    //stringstream fname;
+    //fname << "crankNicolson_" << static_cast<int>(dt*1E6) << ".csv";
     results.save("crankNicolson.csv", arma::csv_ascii);
-
-
-
-  }
+}
 
 
 
@@ -216,9 +252,9 @@ void implicitEuler(bool reflective){
 
 
 int main(){
-  //explicitEuler(true);
-  //implicitEuler(true);
-  crankNicolson(false);
+  //explicitEuler(false);
+  //implicitEuler(false);
+  crankNicolson(true);
   return 0;
 }
 ;
