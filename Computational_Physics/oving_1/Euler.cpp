618dd5a4be49315fdca47d3c9a874a388a61bc2e
Thomas Kleiven <tkleiven@localhost.localdomain>
2017-02-08 22:31:35 +0100

    implemented non-constand diffusion

--- Computational_Physics/oving_1/Euler.cpp
+++ Computational_Physics/oving_1/Euler.cpp
@@ -11,12 +11,12 @@ using namespace std;
 
 
 //Declare variables
-static double endtime = 0.08;
+static double endtime = 0.02;
 
-static int nx = 201;                       //Number of length steps
+static int nx = 401;                       //Number of length steps
 
 static double dx = 1.0/(nx+1);                //Size of length steps
-static double dt = 1/1E5;           //Size of time steps
+static double dt = 1.0/2E7;           //Size of time steps
 
 static int nt = endtime/dt;
 
@@ -51,14 +51,14 @@ public:
   double operator()(double x){
     if(x > 0){return 1.0;}
     else{
-      return 0.5;
+      return 0.1;
     }
   }
 };
 
 
-double getX(int i){
-  return xMin + i*dx;
+double getX(double i){
+  return xMin + (i+1)*dx;
 }
 
 
@@ -87,9 +87,9 @@ void generateA(Scheme_t scheme, Diffusion &D){
       case Scheme_t::CRANK_NICOLSON:
         diagonal(i) = (1+0.5*fPlus+0.5*fMinus);
         diagonalB(i) = (1-0.5*fPlus-0.5*fMinus);
-        if(i < nx-1){
-          subDiagonal(i) = (-fMinus/2);
-          subDiagonalB(i) = fMinus/2;
+        if(i > 0){
+          subDiagonal(i-1) = (-fMinus/2);
+          subDiagonalB(i-1) = fMinus/2;
           }
         break;
         }
@@ -207,7 +207,7 @@ void implicitEuler(bool reflective){
 
     StepDiffusion S;
 
-    generateA(Scheme_t::CRANK_NICOLSON, D);
+    generateA(Scheme_t::CRANK_NICOLSON, S);
 
     setInitialCondition(u);
 
@@ -220,6 +220,7 @@ void implicitEuler(bool reflective){
     //cout << diagonalB(0) << " " << diagonalB(diagonalB.n_elem -1) << endl;
 
 
+    //cout << subDiagonal << endl;
     //Compute factorisation
     dpttrf_(&nx, diagonal.memptr(), subDiagonal.memptr(), &info);
 
@@ -231,19 +232,19 @@ void implicitEuler(bool reflective){
       for(int j=0; j < nx-2; ++j){
         u(j+1) = subDiagonalB(j)*u_l(j) + diagonalB(j+1)*u_l(j+1) + subDiagonalB(j+1)*u_l(j+2);
       }
-      u(0) = u(0)*diagonalB(0) + subDiagonalB(0)*u(1);
-      u(u.n_elem-1) = u(u.n_elem-1)*diagonalB(diagonalB.n_elem-1) + u(u.n_elem-2)*subDiagonalB(subDiagonalB.n_elem-1);
+      u(0) = u_l(0)*diagonalB(0) + subDiagonalB(0)*u_l(1);
+      u(u.n_elem-1) = u_l(u.n_elem-1)*diagonalB(diagonalB.n_elem-1) + u_l(u.n_elem-2)*subDiagonalB(subDiagonalB.n_elem-1);
 
       //Solve the system Ax = Bb
       dpttrs_(&nx, &nrhs, diagonal.memptr(), subDiagonal.memptr(), u.memptr(), &nx,  &info);
 
-      if(abs(0.002-i*dt) <1E-20){
+      if(abs(0.01-i*dt) <1E-20){
         results.col(counter++) = u;
       }
     }
     //stringstream fname;
     //fname << "crankNicolson_" << static_cast<int>(dt*1E6) << ".csv";
-    results.save("crankNicolson.csv", arma::csv_ascii);
+    results.save("crankNicolson_dx.csv", arma::csv_ascii);
 }
 
 
@@ -254,7 +255,7 @@ void implicitEuler(bool reflective){
 int main(){
   //explicitEuler(false);
   //implicitEuler(false);
-  crankNicolson(true);
+  crankNicolson(false);
   return 0;
 }
 ;
